a6e6cf2dab5801d65808ec202c09affd91f5c2b2
tarmes <traarmes@gmail.com>
2020-12-04 11:26:05 -0500

    server running

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const server = express();
 
+server.get('/', (req, res) => {
+   res.send(`<h2>Trevor's Sprint Challenge Submission!!</h2>`)
+})
+
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json())
 
 module.exports = server;
